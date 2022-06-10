7831ed4aae7f2fef0a35f30148c20d3c34bba9ce
Muhammad Hazriq <muhammadhazriq777@gmail.com>
2022-06-10 13:56:58 +0800

    Files with usernames

---
+++ username.py
@@ -0,0 +1,5 @@
+def filter1(url):
+        print("  "+"Files containing username")
+        print("  "+"-------------------------")
+        print(' | site:'+(url)+' inurl:"login="')
+        print("")
