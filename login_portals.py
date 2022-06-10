f4a64ff7231ea36a2dffe883df2ccabcb059be8c
Muhammad Hazriq <muhammadhazriq777@gmail.com>
2022-06-10 13:55:31 +0800

    Files with Login Portals

---
+++ login_portals.py
@@ -0,0 +1,13 @@
+def filter6(url): 
+        print("  "+"Pages Containing Login Portals")
+        print("  "+"------------------------------")
+        print(' | site:"'+(url)+'" inurl:admin/index.php')
+        print(' | inurl:("admin/password.php") +site:'+(url))
+        print(' | site:'+(url)+' inurl:("administrator/login.php" OR "admin/login.php")')
+        print(' | site:conf.'+(url)+'/signin/')
+        print(' | site:login.'+(url)+'/signin/')
+        print(' | site:admin.'+(url)+'/signin/')
+        print(' | site:portal.'+(url)+'/signin/')
+        print(' | site:social.'+(url)+'/signin/')
+        print(' | site:accounts.'+(url)+'/signin/')
+        print("")
