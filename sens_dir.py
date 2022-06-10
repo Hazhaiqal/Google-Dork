67373f0436e3957f82809e6e2b0f25ff542433c5
Muhammad Hazriq <muhammadhazriq777@gmail.com>
2022-06-10 13:56:21 +0800

    Sensitive Directories

---
+++ sens_dir.py
@@ -0,0 +1,10 @@
+def filter3(url):
+        print("  "+"Sensitive Directories")
+        print("  "+"---------------------")
+        print(' | inurl:/database* ext:sql intext:index of -site:'+(url))
+        print(' | inurl:"/drive/folders/" site:'+(url))
+        print(' | site:'+(url)+' shared by')
+        print(' | intitle:index.of ios -site:'+(url))
+        print(' | inurl:"folderview?id=" site:'+(url))
+        print(' | intitle:"index of" "parent directory" "desktop.ini" site:'+(url))
+        print("")
